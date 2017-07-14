9992f5097a02dfc5190339936d797dc6f57cefe5
Ryan Clyde <rclyde@wharton.upenn.edu>
2017-07-14 16:44:58 -0700

    wip

---
+++ backend/routes.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+//
+var models = require('../models.js');
+var User = models.User;
+var Photo = models.Photo;
+
+
+// YOUR API ROUTES HERE
+router.get('/user', function(req, res) {
+  // console.log("finding/creating user");
+  // User.findOne({username: "rcsmooth"}, function(err, user){
+  //   if(err) {console.log(err)}
+  //   else{
+  //     if(user.password === req.body.password){
+  //       res.send({text: "you have just logged in"});
+  //     }
+  //     res.send({text: "helllooooo"})
+  //   }
+  // }) //search for username
+  res.send({text: "this is get user"})
+})
+
+
+router.post('/user', function(req, res) {
+  // var newUser = new User({
+  //   username: req.body.username,
+  //   password: req.body.password,
+  //   friendList: [],
+  //   sentPhotos: [],
+  //   receivedPhotos: []
+  // })
+  res.send({text: "this is post user"})
+})
+
+
+router.post('/photo', function(req, res){
+  // var newPhoto = new Photo({
+  //   from: req.body.username,
+  //   // to: ,
+  //   timestamp: Date,
+  //   labels: Object,
+  //   imgFile: String
+  // })
+  res.send({text: "this is post photo"})
+})
+
+// SAMPLE ROUTE
+router.use('/users', (req, res) => {
+    res.json({ success: true });
+});
+
+module.exports = router;
---
+++ models.js
@@ -0,0 +1,47 @@
+var mongoose = require('mongoose');
+var connect = process.env.MONGODB_URI;
+var findOrCreate = require('mongoose-find-or-create');
+
+// If you're getting an error here, it's probably because
+// your connect string is not defined or incorrect.
+mongoose.connect(connect);
+
+// Step 1: Write your schemas here!
+// Remember: schemas are like your blueprint, and models
+// are like your building!
+var Schema = mongoose.Schema
+
+// var ID = function () {
+//   // Math.random should be unique because of its seeding algorithm.
+//   // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+//   // after the decimal.
+//   return '_' + Math.random().toString(36).substr(2, 9);
+// };
+
+
+var userSchema = new Schema({
+    username: String,
+    password: String,
+    friendList: Array,
+    receivedPhotos: []
+});
+
+var photoSchema = new Schema({
+    from: String,
+    to: String,
+    timestamp: Date,
+    labels: Object,
+    imgFile: String
+});
+
+userSchema.plugin(findOrCreate);
+
+
+var User = mongoose.model('User', userSchema);
+var Photo = mongoose.model('Photo', photoSchema);
+
+
+module.exports = {
+  User: User,
+  Photo: Photo
+};
