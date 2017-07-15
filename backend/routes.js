c8d124da1f0924f17ba610a94a4c09907d406692
Ryan Clyde <rclyde@wharton.upenn.edu>
2017-07-14 18:37:27 -0700

    asdf

--- backend/routes.js
+++ backend/routes.js
@@ -15,7 +15,7 @@ router.get('/user', function(req, res) {
   User.findOne({username: "Ryan"}, function(err, user){
     if(err) {console.log(err)}
     else{
-      res.send(user)
+      res.send({succes: true, user: user})
     }
   }) //search for username
   // res.send({text: "this is get user"})
